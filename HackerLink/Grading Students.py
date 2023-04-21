c00dc2a2c526f1b9280cbe3855f9a07a5a22e1b9
Trilly <tttrillyyy@gmail.com>
2023-04-21 10:47:07 +0900

    HackerLink 3MonPrepKit Week2-2

---
+++ HackerLink/Grading Students.py
@@ -0,0 +1,46 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-grading/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    answer = []
+    for grade in grades:
+        if grade < 38:
+            answer.append(grade)
+        elif (grade//5 * 5 + 5) - grade < 3:
+            answer.append(grade//5 * 5 + 5)
+        elif (grade//5 * 5 + 5) - grade >= 3:
+            answer.append(grade)
+    return answer
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
