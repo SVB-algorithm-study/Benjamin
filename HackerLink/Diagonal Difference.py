891e6a60fc7c406a8c80304169117d52651a7027
Trilly <tttrillyyy@gmail.com>
2023-04-24 12:33:48 +0900

    HackerRank 3monPrepKit Week2-4

---
+++ HackerLink/Diagonal Difference.py
@@ -0,0 +1,43 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-diagonal-difference/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    sum1 = 0
+    sum2 = 0
+    length = len(arr)
+    for i in range(length):
+        sum1 += arr[i][i]
+        sum2 += arr[i][length-1-i]
+    
+    return abs(sum1-sum2)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
