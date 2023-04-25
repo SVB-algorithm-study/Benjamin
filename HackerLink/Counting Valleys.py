d1c2ad1c4e4a0a8e4fd52d82f8a66d5ce0021628
Trilly <tttrillyyy@gmail.com>
2023-04-25 10:44:22 +0900

    HackerLink 3MonPrepKit Week2-56

---
+++ HackerLink/Counting Valleys.py
@@ -0,0 +1,43 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-counting-valleys/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    level = 0
+    cnt = 0
+    for i in range(steps):
+        if path[i] == 'U': level+=1
+        else: 
+            prev_level = level
+            level-=1
+            if level < 0 and prev_level*level == 0: cnt+=1
+    return cnt
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
