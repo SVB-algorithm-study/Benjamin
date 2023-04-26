b6a4e55aa7029b36e7cf947ef83cc37b9a6a4d51
Trilly <tttrillyyy@gmail.com>
2023-04-26 10:13:06 +0900

    HackerRink 3MonPrepKit Week2-7

---
+++ HackerLink/Pangrams.py
@@ -0,0 +1,39 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-pangrams/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    alphabet = [False] * 26
+    for c in s:
+        if c == ' ': continue
+        index = ord(c.lower()) - ord('a')
+        # print(c.lower(), index)
+        alphabet[index] = True
+    
+    if False in alphabet: return "not pangram"
+    else: return "pangram"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
