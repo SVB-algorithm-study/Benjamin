62abfd81ced13f14f12ec7cb6ac4c76df062242a
Trilly <tttrillyyy@gmail.com>
2023-04-28 14:10:22 +0900

    HackerRink 3MonPrepKit Week3-1

---
+++ HackerLink/Mars Exploration.py
@@ -0,0 +1,36 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-mars-exploration/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    # Write your code here
+    cnt = 0
+    for i in range(0,len(s),3):
+        if s[i] != "S": cnt += 1
+        if s[i+1] !="O": cnt += 1
+        if s[i+2] !="S": cnt += 1
+    return cnt
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
