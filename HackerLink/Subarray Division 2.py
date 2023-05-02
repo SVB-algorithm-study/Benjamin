6503252870dd10bba713ad7f616b69b02976b23b
Trilly <tttrillyyy@gmail.com>
2023-05-02 14:02:50 +0900

    HackerLink 3MonPreKits Week3-1

---
+++ HackerLink/Subarray Division 2.py
@@ -0,0 +1,54 @@
+# 
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+
+def birthday(s, d, m):
+    # Write your code here
+    count = 0
+    start = 0
+    w_sum = 0
+    for end in range(len(s)):
+        w_sum += s[end]
+        if end + 1 >= m:
+            if w_sum == d:
+                count += 1
+            w_sum -= s[start]
+            start += 1
+    return count
+            
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
