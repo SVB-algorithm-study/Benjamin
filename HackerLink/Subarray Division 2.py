d407a120648ccca5b86708c4415f03b69b6f5d22
Trilly <tttrillyyy@gmail.com>
2023-05-02 14:05:09 +0900

    HackerLink 3MonPreKits Week3-23

--- HackerLink/Subarray Division 2.py
+++ HackerLink/Subarray Division 2.py
@@ -1,4 +1,4 @@
-# 
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-the-birthday-bar/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-three
 
 #!/bin/python3
 
