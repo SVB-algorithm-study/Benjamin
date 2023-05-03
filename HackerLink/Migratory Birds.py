35667e1c2ca8585c1b5cf44c03e4cc4447fad691
Trilly <tttrillyyy@gmail.com>
2023-05-03 13:03:37 +0900

    HackerLink 3MonPreKits Week3-4

---
+++ HackerLink/Migratory Birds.py
@@ -0,0 +1,42 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-migratory-birds/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-three
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+from collections import Counter
+
+def migratoryBirds(arr):
+    # Write your code here
+    
+    # return Counter(arr).most_common(1)[0][0]
+    
+    types = [0] * 6
+    for t in arr:
+        types[t] += 1
+        
+    return types.index(max(types))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
--- HackerLink/Sales by Match.py
+++ HackerLink/Sales by Match.py
@@ -1,4 +1,4 @@
-# 
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-sock-merchant/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-three
 
 #!/bin/python3
 
