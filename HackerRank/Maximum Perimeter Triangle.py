bbaf86ad1d64c04936a8e415c31553e1db65ded1
Trilly <tttrillyyy@gmail.com>
2023-05-10 14:32:22 +0900

    HackerRank 3MonPreKit Week3-6

---
+++ HackerRank/Maximum Perimeter Triangle.py
@@ -0,0 +1,39 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-maximum-perimeter-triangle/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-three
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximumPerimeterTriangle' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY sticks as parameter.
+#
+
+def maximumPerimeterTriangle(sticks):
+    # Write your code here
+    sticks.sort()
+    for i in range(len(sticks)-1,1,-1):
+        a,b,c = sticks[i-2], sticks[i-1], sticks[i]
+        print(a,b,c)
+        if c<a+b: return [a,b,c]
+    return [-1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    sticks = list(map(int, input().rstrip().split()))
+
+    result = maximumPerimeterTriangle(sticks)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
