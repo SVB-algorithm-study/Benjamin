e564fa72d51f415318c378d10cede910bfe4d057
Trilly <tttrillyyy@gmail.com>
2023-05-10 17:06:11 +0900

    HackerRank 3MonPrepKit Week3 mocktest

---
+++ HackerRank/Between Two Sets.py
@@ -0,0 +1,44 @@
+# https://www.hackerrank.com/challenges/between-two-sets/problem?h_r=internal-search
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def getTotalX(a, b):
+    # Write your code here
+    answer = 0
+    for e in range(a[-1], b[-1]+1, a[-1]):
+        if all(e%x==0 for x in a) and all(y%e==0 for y in b): answer += 1
+    return answer
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
