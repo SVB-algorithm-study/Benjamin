0ceb308bec1c716547eafa25e2ed91a1b2ed379b
Trilly <tttrillyyy@gmail.com>
2023-05-12 13:15:20 +0900

    HakerRank 3MonPrepKit Week4-1

---
+++ HackerRank/Picking Numbers.py
@@ -0,0 +1,47 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-picking-numbers/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-four
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    # Write your code here
+    
+    strg = [0 for _ in range(100)]
+    for n in a:
+        strg[n] += 1
+    
+    longest = 0
+    for i in range(1,99):
+        temp = strg[i]+strg[i+1]
+        if longest < temp:
+            longest = temp
+    
+    return longest
+            
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
