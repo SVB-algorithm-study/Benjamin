1a82bdd9a458afa83824c465360e6978daa49202
Trilly <tttrillyyy@gmail.com>
2023-05-16 13:38:19 +0900

    HackerRank 3MonPrepKit Week4-7

---
+++ HackerRank/Minimum Absolute Difference in an Array.py
@@ -0,0 +1,39 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-minimum-absolute-difference-in-an-array/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-four
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumAbsoluteDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def minimumAbsoluteDifference(arr):
+    # Write your code here
+    arr.sort()
+    length = len(arr)
+    min_diff = 10**9*2
+    for i in range(1,length):
+        min_diff = min(min_diff, arr[i]-arr[i-1])
+    return min_diff
+        
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
