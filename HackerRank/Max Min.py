863f467642085210bba74efd839e3c108583134f
Trilly <tttrillyyy@gmail.com>
2023-05-22 14:23:59 +0900

    HackerRank 3MonPrepKit Week5-1

---
+++ HackerRank/Max Min.py
@@ -0,0 +1,50 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-angry-children/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-five
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maxMin' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def maxMin(k, arr):
+    # Write your code here
+    arr.sort()
+    # print(arr)
+    length = len(arr)
+    minimum = 10**9
+    for i in range(0,length-k+1):
+        minimum = min(minimum, arr[i+k-1] - arr[i])
+        if minimum == 0: break
+        # print(arr[i], arr[i+k])
+    
+    return minimum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    k = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input().strip())
+        arr.append(arr_item)
+
+    result = maxMin(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
